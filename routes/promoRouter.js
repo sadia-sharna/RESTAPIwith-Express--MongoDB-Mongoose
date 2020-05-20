6c6164a76b2d2013e46607d2dbd98b45539626c9
SadiaGithub <s.sharna06@gmail.com>
2020-05-20 22:23:03 +0600

    Expres Generator

---
+++ routes/promoRouter.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+promoRouter.use(bodyParser.json());
+
+
+promoRouter.route('/') 
+.all( (req,res,next)=> { 
+    
+    res.statusCode =200;                              
+    res.setHeader('Content-Type', 'text/plain');  
+    next();                        
+
+})
+.get( (req,res,next) => {
+res.end("Will send all the promos to you");
+
+})
+.post((req,res,next) => {
+
+res.end("Will add the promo: " + req.body.name +
+"Desc "+req.body.description);
+
+})
+.put( (req,res,next) => {
+req.statusCode = 403;
+res.end('PUT operation not support on promos');
+})
+.delete((req,res,next) => {
+res.end('Deleting all the promos');
+
+});
+
+promoRouter.route('/:promoId')
+.get( (req,res,next) => {
+    res.end("Will send details of the promo: " +
+    req.params.promoId + ' to you'); 
+    
+
+})
+.post((req,res,next) => {
+    
+    req.statusCode = 403;
+    res.end('Post operation not support on promos');
+
+})
+.put((req,res,next) => {
+    
+    res.write('Updating the promo' + ' '+ req.params.promoId +'\n');
+    res.end('Will update the promo: '+
+    req.body.name +
+    ' with details '+ req.body.description);
+
+})
+.delete( (req,res,next) => {
+    res.end('Deleting promo '+ req.params.promoId);
+    
+    });
+
+module.exports = promoRouter;
